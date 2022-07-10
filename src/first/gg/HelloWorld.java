9f1593e52d749f8d397a76706c1ec3fcebc66604
Yan Lukavii <mr.winston93@mail.ru>
2022-07-10 14:29:20 +0300

    Hello an GoodBye

---
+++ src/first/gg/HelloWorld.java
@@ -0,0 +1,8 @@
+package first.gg;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/second/gg/GoodByeWorld.java
@@ -0,0 +1,8 @@
+package second.gg;
+
+public class GoodByeWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Good bye World!!");
+    }
+}
